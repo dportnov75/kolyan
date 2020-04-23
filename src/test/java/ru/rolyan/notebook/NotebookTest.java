26c366747b4b4e9e794a3ff0b79397f17bd2c515
dportnov <dportnov@DESKTOP-MEJQ0HC>
2020-04-23 19:20:08 +0300

    1

--- src/test/java/ru/rolyan/notebook/NotebookTest.java
+++ src/test/java/ru/rolyan/notebook/NotebookTest.java
@@ -24,15 +24,19 @@ public class NotebookTest {
 		List<Contact> contacts = notebook.getContacts();
 		Assertions.assertTrue(contacts.size() == 1);
 	}
-}
+}//Эта скобка закрывает класс
+//И этот метод висит в воздухе
     @Test
     public void test_notebook2() {
 	   Notebook notebook = new Notebook();
-	   Contact vasya = new Contact("a","d","s");
+	   Contact vasya = new Contact("Сысой","d","s");
 	   Contact vasya1 = new Contact("b","d","s");
 	   notebook.addContact(vasya);
 	   notebook.addContact(vasya1);
 	   
 	   List<Contact> contacts = notebook.getContacts();
+	   //Ты добавил два контакта. Этот метод вернет false и тест не пройдет
 	   Assertions.assertTrue(contacts.size() == 1);
+	   //Вот пример теста
+	   Assertions.assertTrue(notebook.findByName("Сысой")!= null);
 }
